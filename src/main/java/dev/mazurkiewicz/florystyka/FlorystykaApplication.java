e72c540e69caf5aa33daf35cf5d1a955b4d2a62f
Marcin Mazurkiewicz <marcin@mazurkiewicz.dev>
2020-04-30 07:26:48 +0200

    Java init fresh start

---
+++ src/main/java/dev/mazurkiewicz/florystyka/FlorystykaApplication.java
@@ -0,0 +1,13 @@
+package dev.mazurkiewicz.florystyka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlorystykaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlorystykaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/mazurkiewicz/florystyka/FlorystykaApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.mazurkiewicz.florystyka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlorystykaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
