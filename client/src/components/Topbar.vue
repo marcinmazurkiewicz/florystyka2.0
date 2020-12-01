1de2026e4d4c9667c347a5af7c054fa95212e90c
Marcin Mazurkiewicz <marcin@mazurkiewicz.dev>
2020-12-01 15:07:54 +0100

    Added topbar component. Only a visual prototype!

---
+++ client/src/components/Topbar.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="w-full sticky top-0 bg-light-green flex justify-between border-b-2 border-green">
+    <p class="p-4 text-xl">Testy florystyczne</p>
+    <nav class="w-full max-w-screen-lg text-right m-auto">
+      <ul class="uppercase">
+        <li class="inline-block p-4 hover:bg-black-01 cursor-pointer">Start</li>
+        <li class="inline-block p-4 hover:bg-black-01 cursor-pointer">Test</li>
+        <li class="inline-block p-4 hover:bg-black-01 cursor-pointer">Szybko</li>
+        <li class="inline-block p-4 hover:bg-black-01 cursor-pointer">Pdf</li>
+        <li class="inline-block p-4 hover:bg-black-01 cursor-pointer">Info</li>
+      </ul>
+    </nav>
+  </div>
+</template>
