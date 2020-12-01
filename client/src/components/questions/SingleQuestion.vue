9e07bafd8b6aaf60f012a0a888eae8e399551def
Marcin Mazurkiewicz <marcin@mazurkiewicz.dev>
2020-12-01 21:27:21 +0100

    Single question view mockup

---
+++ client/src/components/questions/SingleQuestion.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="w-full max-w-screen-lg mx-auto text-white">
+    <header>
+      <h1 class="text-center text-3xl py-6 text-red">Szybkie pytanie</h1>
+      <p class="text-sm text-center">Poćwicz pojedyncze pytania. Od razu poznasz prawidłową idpowiedź, co pozwoli na jej
+        łatwiejsze zapamiętanie.</p>
+    </header>
+    <section>
+      <p class="py-4 px-5 mt-5 mb-1 bg-dark-gray font-bold">Zasuszone metodą zielnikową liście Ginkgo biloba należy
+        przechowywać w pomieszczeniu:</p>
+      <span class="block py-3 px-5 mb-1 bg-gray border border-dark-gray hover:text-azure hover:border-azure">A. suchym, przewiewnym i w kartonach</span>
+      <span class="block py-3 px-5 mb-1 bg-gray border border-dark-gray hover:text-azure hover:border-azure">B. lekko wilgotnym i w workach foliowych</span>
+      <span class="block py-3 px-5 mb-1 bg-gray border border-dark-gray hover:text-azure hover:border-azure">C. chłodnym i wyłącznie na stojąco</span>
+      <span class="block py-3 px-5 mb-1 bg-gray border border-dark-gray hover:text-azure hover:border-azure">D. ciemnym, wilgotnym i zawieszone</span>
+      <span class="block py-2 px-5 mb-1 bg-dark-gray text-xs text-right ">Pyt. 439</span>
+    </section>
+    <button
+        class="w-full bg-light-green mt-8 p-3 text-dark-gray text-lg font-semibold border border-green rounded hover:bg-green hover:text-white">
+      Sprawdź
+    </button>
+  </div>
+</template>
