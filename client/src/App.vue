c4c8b8f328cbd0c798d58a724f3837177dc904c6
Marcin Mazurkiewicz <marcin@mazurkiewicz.dev>
2020-12-01 15:08:48 +0100

    Added styles, topbar and footer to main app component

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,12 +1,20 @@
 <template>
-  <div>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <router-view />
+  <div class="w-screen h-screen bg-deep-cyan font-lato overflow-y-auto">
+    <Topbar/>
+    <router-view/>
+    <footer class="w-full bg-deep-cyan text-white text-right text-sm px-6 mt-6 py-4">
+      Florystyka 3.0.1 &copy; 2020 <a class="text-red" href="https://mazurkiewicz.dev">Marcin Mazurkiewicz</a>
+    </footer>
   </div>
 </template>
 
 <script>
+import Topbar from '@/components/Topbar'
+
 export default {
   name: 'App',
+  components: {
+    Topbar
+  }
 }
 </script>
