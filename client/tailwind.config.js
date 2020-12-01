21f270862b0e397445c0eacc3a2dfd2c8f109094
Marcin Mazurkiewicz <marcin@mazurkiewicz.dev>
2020-12-01 15:06:44 +0100

    Added necessary styles and images

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,11 +1,26 @@
 module.exports = {
-  purge: [],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
+    purge: [],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        extend: {
+            colors: {
+                'light-green': '#8bc34a',
+                'green': '#71a436',
+                'deep-cyan': '#161B20',
+                'black-01': 'rgba(0,0,0,0.1)',
+                'black-05': 'rgba(0,0,0,0.5)',
+                'red': '#ff5252'
+            },
+            fontFamily: {
+                'lato': 'Lato, sans-serif',
+            },
+            backgroundImage: {
+                'start-img': "url('img/start.png')"
+            },
+        },
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
 }
