a307b1fa79153b37393ea9834bb19bcc705d07e7
Marcin Mazurkiewicz <marcin@mazurkiewicz.dev>
2020-12-01 15:09:09 +0100

    Added main page

---
+++ client/src/components/Home.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <header class="w-full bg-start-img bg-cover" style="background-position: 50% 40%;">
+      <div class="w-full h-full bg-black-05">
+        <h1 class="text-center text-white font-bold text-6xl pt-36">Kwalifikacja R.26</h1>
+        <h5 class="text-center text-white text-3xl pt-8 pb-36">Wykonywanie kompozycji florystycznych</h5>
+      </div>
+    </header>
+    <main class="w-full max-w-screen-lg m-auto text-white">
+      <section>
+        <header class="text-center text-4xl pt-16 pb-8 px-6 tracking-wider leading-relaxed">
+          Przygotowujesz się do egzaminu kwalifikacyjnego z florystyki? Poćwicz testy!
+        </header>
+
+        <p class="text-justify tracking-wide leading-loose">W bazie znajdują się pytania z arkuszy egzaminacyjnych
+          z lat <span class="text-red">2010 - 2016</span>. Łączna liczba pytań w bazie to
+          obecnie <span class="text-red">550</span>. Najnowsze pytania pochodzą z
+          egzaminu z <span class="text-red">czerwca 2016 roku</span>. Testy możesz
+          rozwiązywać na trzy sposoby:
+        </p>
+
+        <div class="py-6 w-full grid grid-cols-3">
+          <div class="text-center p-6">
+            <h5 class="text-red text-2xl">Symulacja testu</h5>
+            <i class="material-icons text-7xl text-light-green py-6">receipt</i>
+            <p class="text-justify">Losowane jest 40 pytań, na rozwiązanie których jest 60 minut. Po upływie
+              czasu test zostaje przerwany, tak jak na prawdziwym egzaminie.</p>
+          </div>
+          <div class="text-center p-6">
+            <h5 class="text-red text-2xl">Szybki test</h5>
+            <i class="material-icons text-7xl text-light-green py-6">av_timer</i>
+            <p class="text-justify">Opcja dla zabieganych. Losowane jest jedno pytanie, które od razu można
+              sprawdzić.</p>
+          </div>
+          <div class="text-center p-6">
+            <h5 class="text-red text-2xl">PDF</h5>
+            <i class="material-icons text-7xl text-light-green py-6">print</i>
+            <p class="text-justify">Przydatne dla nauczycieli. Generuje gotowy do druku test z 40 pytaniami i
+              kluczem odpowiedzi.</p>
+          </div>
+        </div>
+
+        <p class="text-justify tracking-wide leading-loose pb-8">
+          Jeżeli znajdziesz na stronie jakiś błąd, będę wdzięczny, jeżeli zgłosisz do mailowo na adres
+          <a class="text-red" href="mailto:marcin@mazurkiewicz.dev">marcin@mazurkiewicz.dev</a>. Przy
+          opisie błędu podaj numer pytania, który znajduje się w szarym pasku pod każdym pytaniem. Pomoże
+          mi to szybko i bezproblemowo poprawić wszystkie niedociągnięcia.</p>
+      </section>
+
+    </main>
+  </div>
+</template>
