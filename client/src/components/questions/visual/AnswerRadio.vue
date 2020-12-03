da5d2a3cce06bc4951082024a7bd1c6e6f563f06
Marcin Mazurkiewicz <marcin@mazurkiewicz.dev>
2020-12-03 15:20:03 +0100

    Sending answers to the backend and displaying the result

--- client/src/components/questions/visual/AnswerRadio.vue
+++ client/src/components/questions/visual/AnswerRadio.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
-    <label :for="id" :class="[isChecked ? 'text-azure border-azure' : 'text-white border-dark-gray']"
-           class="block w-full py-3 px-5 mb-1 bg-gray border hover:text-azure hover:border-azure">
+    <label :for="id" :class="color"
+           class="block w-full py-3 px-5 mb-1 bg-gray border">
       <slot/>
       <input
           :name="name"
@@ -19,11 +19,30 @@
 
 export default {
   name: 'AnswerRadio',
-  props: ['name', 'id', 'value', 'modelValue'],
+  props: ['name', 'id', 'value', 'modelValue', 'solution'],
   computed: {
     isChecked() {
       return this.value === this.modelValue;
     },
+    color() {
+      const defaultStyle = 'text-white border-dark-gray hover:text-azure hover:border-azure';
+      const checked = 'text-azure border-azure';
+      const correct = 'text-light-green border-light-green';
+      const incorrect = 'text-red border-red';
+      if (this.solution.correct != null) {
+        if (this.isCorrect) {
+          return correct;
+        } else if (this.isChecked) {
+          return incorrect;
+        }
+      } else if (this.isChecked) {
+        return checked;
+      }
+      return defaultStyle;
+    },
+    isCorrect() {
+      return this.solution.correct === this.value;
+    }
   }
 }
 </script>
---
+++ client/src/components/questions/visual/AnswerSpan.vue
@@ -0,0 +1,34 @@
+<template>
+  <div :id="id" :class="color"
+       class="block w-full py-3 px-5 mb-1 bg-gray border">
+    <slot/>
+  </div>
+</template>
+<script>
+
+export default {
+  name: 'AnswerSpan',
+  props: ['id', 'value', 'checked', 'solution'],
+  computed: {
+    color() {
+      const defaultStyle = 'text-white border-dark-gray';
+      const checked = 'text-azure border-azure';
+      const correct = 'text-light-green border-light-green';
+      const incorrect = 'text-red border-red';
+      if (this.solution.correct != null) {
+        if (this.isCorrect) {
+          return correct;
+        } else if (this.checked) {
+          return incorrect;
+        }
+      } else if (this.checked) {
+        return checked;
+      }
+      return defaultStyle;
+    },
+    isCorrect() {
+      return this.solution.correct === this.value;
+    }
+  }
+}
+</script>
