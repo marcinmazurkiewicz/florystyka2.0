b33abc025b96a63c3b2dffe072169d76822cf9f4
Marcin Mazurkiewicz <marcin@mazurkiewicz.dev>
2020-12-03 11:46:54 +0100

    Adjustment question component for displaying tables and pictures

--- client/src/components/questions/visual/Question.vue
+++ client/src/components/questions/visual/Question.vue
@@ -1,12 +1,12 @@
 <template>
   <section>
     <p :id="question.id" class="py-4 px-5 mt-5 mb-1 bg-dark-gray font-bold">{{ number + 1 }}.
-      {{ question.content }}</p>
+      <span v-html="question.content"></span>
+      <img v-if="question.img" :src="imgUrl" class="pt-4 mx-auto" alt="obrazek do pytania"/>
+    </p>
     <div v-for="answer in question.answers" :key="answer.value">
       <answer-radio :value="answer.value" :id='question.id +"_"+answer.value' :name='question.id+"_answer"'
-                    v-model="localSelectedAnswer">{{
-          answer.content
-        }}
+                    v-model="localSelectedAnswer"><span v-html="answer.content"></span>
       </answer-radio>
     </div>
     <span class="block py-2 px-5 mb-1 bg-dark-gray text-xs text-right ">Pyt. {{ question.id }}</span>
@@ -43,8 +43,19 @@ export default {
       set(localSelectedAnswer) {
         this.$emit('update:modelValue', localSelectedAnswer);
       }
-
+    },
+    imgUrl() {
+      return require(`@/assets/quest_img/${this.question.img}`);
     }
   }
 }
 </script>
+<style>
+table {
+  @apply w-full p-4 table font-light mt-4;
+}
+
+td, th {
+  @apply table-cell p-2 border border-dark-green text-sm;
+}
+</style>
