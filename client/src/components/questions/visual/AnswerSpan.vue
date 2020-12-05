195d04e4e9949ad889ec976fdfe226aafe65f7e3
Marcin Mazurkiewicz <marcin@mazurkiewicz.dev>
2020-12-05 14:24:48 +0100

    Changed display answers in solved questions

--- client/src/components/questions/visual/AnswerSpan.vue
+++ client/src/components/questions/visual/AnswerSpan.vue
@@ -1,7 +1,11 @@
 <template>
   <div :id="id" :class="color"
-       class="block w-full py-3 px-5 mb-1 bg-gray border">
-    <slot/>
+       class="block w-full py-3 px-5 mb-1 bg-gray border flex justify-between items-center">
+    <slot class="text-gr"/>
+    <div v-if="checked" class="w-6 h-6 flex-shrink-0">
+      <img v-if="isCorrect" src="https://s.svgbox.net/hero-outline.svg?ic=check&fill=8bc34a" class="w-full" alt="correct"/>
+      <img v-else src="https://s.svgbox.net/hero-outline.svg?ic=x&fill=ff5252" class="w-full" alt="incorrect"/>
+    </div>
   </div>
 </template>
 <script>
@@ -11,20 +15,21 @@ export default {
   props: ['id', 'value', 'checked', 'solution'],
   computed: {
     color() {
-      const defaultStyle = 'text-white border-dark-gray';
-      const checked = 'text-azure border-azure';
-      const correct = 'text-light-green border-light-green';
-      const incorrect = 'text-red border-red';
-      if (this.solution.correct != null) {
-        if (this.isCorrect) {
-          return correct;
-        } else if (this.checked) {
-          return incorrect;
-        }
+      const uncheckedFont = 'italic';
+      const checkedFont = 'font-semibold not-italic';
+      const uncheckedColor = 'text-gray-400 border-dark-gray';
+      const correctColor = 'text-light-green border-light-green';
+      const incorrectColor = 'text-red border-red';
+
+      let style = this.checked ? checkedFont : uncheckedFont;
+      if (this.isCorrect) {
+        style += ` ${correctColor}`;
       } else if (this.checked) {
-        return checked;
+        style += ` ${incorrectColor}`;
+      } else {
+        style += ` ${uncheckedColor}`;
       }
-      return defaultStyle;
+      return style;
     },
     isCorrect() {
       return this.solution.correct === this.value;
