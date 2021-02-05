c6ad296a6522c2943bad7679349313a58e9a5579
Marcin Mazurkiewicz <marcin@mazurkiewicz.dev>
2021-02-05 14:12:17 +0100

    Added user entity, controller, service and repository

---
+++ src/main/java/dev/mazurkiewicz/florystyka/user/User.java
@@ -0,0 +1,54 @@
+package dev.mazurkiewicz.florystyka.user;
+
+import dev.mazurkiewicz.florystyka.auth.Authority;
+import lombok.Data;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Set;
+
+@Data
+@Entity(name = "users")
+public class User implements UserDetails, Serializable {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    @Column(nullable = false, unique = true)
+    private String email;
+    @Column(nullable = false)
+    private String password;
+    private boolean isAccountExpired;
+    private boolean isAccountLocked;
+    private boolean isCredentialsExpired;
+    private boolean isEnabled;
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "user_authority",
+            joinColumns = {@JoinColumn(name = "user_id", referencedColumnName = "id")},
+            inverseJoinColumns = {@JoinColumn(name = "auth_id", referencedColumnName = "id")})
+    private Set<Authority> authorities;
+
+    @Override
+    public String getUsername() {
+        return getEmail();
+    }
+
+    @Override
+    @Transient
+    public boolean isAccountNonExpired() {
+        return !isAccountExpired;
+    }
+
+    @Override
+    @Transient
+    public boolean isAccountNonLocked() {
+        return !isAccountLocked;
+    }
+
+    @Override
+    @Transient
+    public boolean isCredentialsNonExpired() {
+        return !isCredentialsExpired;
+    }
+}
