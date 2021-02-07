b708ac1bbbd299ae38b955082ba40b07bb7229ea
Marcin Mazurkiewicz <marcin@mazurkiewicz.dev>
2021-02-07 21:38:56 +0100

    Added component to preview exist questions and create new

--- client/src/router.js
+++ client/src/router.js
@@ -1,5 +1,6 @@
 import {createWebHistory, createRouter} from "vue-router";
 import {trackRouter} from "vue-gtag-next";
+import {hasAnyRight, isLoggedUser} from "@/auth/authUtils";
 
 const Home = () => import("@/components/HomeView.vue");
 const Info = () => import("@/components/InfoView.vue");
@@ -8,43 +9,125 @@ const SingleQuestion = () => import("@/components/questions/SingleQuestion");
 const RandomQuestion = () => import("@/components/questions/RandomQuestion");
 const Test = () => import("@/components/questions/Test");
 const PrivacyPolicy = () => import("@/components/privacy/PrivacyPolicyView")
+const Login = () => import("@/components/LoginView")
+const AdminDashboard = () => import("@/components/admin/Dashboard")
+const AdminQuestionsView = () => import("@/components/admin/AdminQuestionsView")
+const Unauthorized = () => import("@/components/UnauthorizedView")
+const AdminQuestionView = () => import("@/components/admin/AdminQuestionView")
+const AddEditQuestion = () => import("@/components/admin/AddEditQuestion")
 
 const routes = [
     {
         path: "/",
         name: "Home",
         component: Home,
+        meta: {
+            requiresAuth: false
+        }
     },
     {
         path: "/single",
         name: "RandomQuestion",
-        component: RandomQuestion
+        component: RandomQuestion,
+        meta: {
+            requiresAuth: false
+        }
 
     },
     {
         path: "/question/:questionId",
         name: "SingleQuestion",
-        component: SingleQuestion
+        component: SingleQuestion,
+        meta: {
+            requiresAuth: false
+        }
     },
     {
         path: "/info",
         name: "Info",
-        component: Info
+        component: Info,
+        meta: {
+            requiresAuth: false
+        }
     },
     {
         path: "/test",
         name: "Test",
-        component: Test
+        component: Test,
+        meta: {
+            requiresAuth: false
+        }
     },
     {
         path: "/pdf",
         name: "Pdf",
-        component: Pdf
+        component: Pdf,
+        meta: {
+            requiresAuth: false
+        }
     },
     {
         path: "/privacy",
         name: "PrivacyPolicy",
-        component: PrivacyPolicy
+        component: PrivacyPolicy,
+        meta: {
+            requiresAuth: false
+        }
+    },
+    {
+        path: "/login",
+        name: "Login",
+        component: Login,
+        meta: {
+            requiresAuth: false
+        }
+    },
+    {
+        path: "/admin",
+        name: "AdminDashboard",
+        component: AdminDashboard,
+        meta: {
+            requiresAuth: true,
+            allowedFor: ['ROLE_ADMIN']
+        },
+        children: [
+            {
+                path: "questions",
+                name: "AdminQuestionsView",
+                component: AdminQuestionsView,
+                meta: {
+                    requiresAuth: true,
+                    allowedFor: ['ROLE_ADMIN']
+                },
+            },
+            {
+                path: "questions/add",
+                name: "AddQuestion",
+                component: AddEditQuestion,
+                meta: {
+                    requiresAuth: true,
+                    allowedFor: ['ROLE_ADMIN']
+                },
+            },
+            {
+                path: "questions/:questionId",
+                name: "AdminQuestionView",
+                component: AdminQuestionView,
+                meta: {
+                    requiresAuth: true,
+                    allowedFor: ['ROLE_ADMIN']
+                },
+            },
+            {
+                path: 'unauthorized',
+                name: 'Unauthorized',
+                component: Unauthorized,
+                props: true,
+                meta: {
+                    requiresAuth: true
+                }
+            },
+        ]
     }
 ];
 
@@ -52,6 +135,30 @@ const router = createRouter({
     history: createWebHistory(),
     routes,
 });
+
+router.beforeEach((to, from, next) => {
+    if (to.matched.some(record => record.meta.requiresAuth)) {
+        if (!isLoggedUser()) {
+            next({
+                name: 'Login',
+                params: {nextUrl: to.fullPath}
+            })
+        } else if (to.matched.some(record => record.meta.allowedFor)) {
+            if (hasAnyRight(to.meta.allowedFor)) {
+                next()
+            } else {
+                next({
+                    name: 'Unauthorized',
+                    params: {nextUrl: to.fullPath}
+                })
+            }
+        } else {
+            next();
+        }
+    } else {
+        next();
+    }
+})
 
 trackRouter(router);
 export default router;
