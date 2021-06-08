398c4a3368b01977aec12b3044e1501e6883b154
Marcin Mazurkiewicz <marcin@mazurkiewicz.dev>
2021-06-08 21:07:35 +0200

    Extended PreparedResponse type for headers field

--- client/src/types/PreparedResponse.ts
+++ client/src/types/PreparedResponse.ts
@@ -1,6 +1,11 @@
 import { ResponseStatus } from "@/types/ResponseStatus";
 
+export type Header = {
+  [key: string]: string;
+};
+
 export type PreparedResponse<T> = {
   data?: T;
   responseStatus: ResponseStatus;
+  headers?: Header[];
 };
