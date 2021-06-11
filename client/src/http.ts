22a20269e43ff8c851bdf37b4299f3c4805b9b94
Marcin Mazurkiewicz <marcin@mazurkiewicz.dev>
2021-06-11 12:14:23 +0200

    Added axios interceptor to added authorization header in request

--- client/src/http.ts
+++ client/src/http.ts
@@ -1,7 +1,20 @@
 import axios from "axios";
+import _ from "lodash";
+import { getToken } from "@/utils/authUtils";
 
 const HTTP = axios.create({
   baseURL: process.env.VUE_APP_API_URL
 });
+
+HTTP.interceptors.request.use(
+  config => {
+    const originalRequest = _.cloneDeep(config);
+    if (getToken()) {
+      _.set(originalRequest, "headers.Authorization", getToken());
+    }
+    return originalRequest;
+  },
+  err => Promise.reject(err)
+);
 
 export { HTTP };
