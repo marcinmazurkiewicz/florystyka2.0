c4ea79843acfc6f0a77c98b7d80c78b75e1a936f
Marcin Mazurkiewicz <marcin@mazurkiewicz.dev>
2021-06-12 15:15:08 +0200

    Added new routes and requiresAuth parameter for all routes

--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -1,45 +1,99 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
+import { trackRouter } from "vue-gtag-next";
+import { isLoggedUser, hasAnyRight } from "@/utils/authUtils";
 
-const routes: Array<RouteRecordRaw> = [
+const routes: RouteRecordRaw[] = [
   {
     path: "/",
     name: "Home",
-    component: () => import("@/views/HomeView.vue")
+    component: () => import("@/views/HomeView.vue"),
+    meta: {
+      requiresAuth: false
+    }
   },
   {
     path: "/random",
     name: "RandomQuestion",
-    component: () => import("@/views/RandomQuestionView.vue")
+    component: () => import("@/views/RandomQuestionView.vue"),
+    meta: {
+      requiresAuth: false
+    }
   },
   {
     path: "/question/:questionId",
     name: "SingleQuestion",
-    component: () => import("@/views/SingleQuestionView.vue")
+    component: () => import("@/views/SingleQuestionView.vue"),
+    meta: {
+      requiresAuth: false
+    }
   },
   {
     path: "/info",
     name: "Info",
-    component: () => import("@/views/InfoView.vue")
+    component: () => import("@/views/InfoView.vue"),
+    meta: {
+      requiresAuth: false
+    }
   },
   {
     path: "/test",
     name: "Test",
-    component: () => import("@/views/TestView.vue")
+    component: () => import("@/views/TestView.vue"),
+    meta: {
+      requiresAuth: false
+    }
   },
   {
     path: "/pdf",
     name: "Pdf",
-    component: () => import("@/views/PdfView.vue")
+    component: () => import("@/views/PdfView.vue"),
+    meta: {
+      requiresAuth: false
+    }
   },
   {
     path: "/privacy",
     name: "PrivacyPolicy",
-    component: () => import("@/views/PrivacyPolicyView.vue")
+    component: () => import("@/views/PrivacyPolicyView.vue"),
+    meta: {
+      requiresAuth: false
+    }
   },
   {
     path: "/login",
     name: "Login",
-    component: () => import("@/views/LoginView.vue")
+    component: () => import("@/views/LoginView.vue"),
+    meta: {
+      requiresAuth: false
+    }
+  },
+  {
+    path: "/unauthorized",
+    name: "Unauthorized",
+    component: () => import("@/views/UnauthorizedView.vue"),
+    meta: {
+      requiresAuth: false
+    }
+  },
+  {
+    path: "/dashboard",
+    name: "DashboardView",
+    component: () => import("@/views/logged/DashboardView.vue"),
+    meta: {
+      requiresAuth: true,
+      allowedFor: ["ROLE_ADMIN"]
+    },
+    children: [
+      {
+        path: "/questions",
+        name: "AdminQuestionsView",
+        component: () => import("@/views/logged/admin/AdminQuestionsView.vue"),
+        meta: {
+          requiresAuth: true,
+          allowedFor: ["ROLE_ADMIN"]
+        }
+      }
+    ]
   }
 ];
 
@@ -47,9 +101,33 @@ const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes,
   scrollBehavior() {
-    // always scroll to top
     return { top: 0 };
   }
 });
 
+router.beforeEach((to, from, next) => {
+  if (to.matched.some(record => record.meta.requiresAuth)) {
+    if (!isLoggedUser()) {
+      next({
+        name: "Login",
+        params: { nextUrl: to.fullPath }
+      });
+    } else if (to.matched.some(record => record.meta.allowedFor)) {
+      if (hasAnyRight(to.meta.allowedFor)) {
+        next();
+      } else {
+        next({
+          name: "Unauthorized",
+          params: { nextUrl: to.fullPath }
+        });
+      }
+    } else {
+      next();
+    }
+  } else {
+    next();
+  }
+});
+
+trackRouter(router);
 export default router;
