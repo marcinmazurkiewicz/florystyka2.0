80504ab383bc166c394eac973d72bee2c455c2aa
Marcin Mazurkiewicz <marcin@mazurkiewicz.dev>
2021-06-20 04:49:02 +0200

    Added composable register method

--- client/src/composables/useAuthorization.ts
+++ client/src/composables/useAuthorization.ts
@@ -1,22 +1,38 @@
-import { LoginRequest, LoginResponse } from "@/types/AuthTypes";
-import { ref, Ref } from "vue";
-import { postRequest } from "@/services/apiService";
-import { ResponseStatus } from "@/types/ResponseStatus";
+import {
+  LoginRequest,
+  RegisterRequest,
+  LoginResponse
+} from "@/types/AuthTypes";
+
 import { Header, PreparedResponse } from "@/types/PreparedResponse";
 import { login } from "@/utils/authUtils";
+import { sendLoginRequest } from "@/services/authorizationService";
 
 export function useAuthorization() {
-  const responseStatus: Ref<ResponseStatus> = ref({
-    isDataReturned: true,
-    isError: false,
-    isPending: false,
-    errors: {}
-  });
-
-  async function sendLoginRequest(
+  function tryLogin(
     loginRequest: LoginRequest
   ): Promise<PreparedResponse<LoginResponse>> {
-    return postRequest<LoginResponse>("api/v3/auth/login", loginRequest)
+    return sendLoginRequest(loginRequest)
+      .then(response => {
+        if (response.headers) {
+          const token: string | undefined =
+            response.headers[Header.AUTHORIZATION];
+          if (token != undefined) login(token);
+        }
+        return response;
+      })
+      .catch(errorStatus => {
+        const response: PreparedResponse<LoginResponse> = {
+          responseStatus: errorStatus
+        };
+        return response;
+      });
+  }
+
+  function tryRegister(
+    loginRequest: RegisterRequest
+  ): Promise<PreparedResponse<LoginResponse>> {
+    return sendLoginRequest(loginRequest)
       .then(response => {
         if (response.headers) {
           const token: string | undefined =
@@ -34,7 +50,7 @@ export function useAuthorization() {
   }
 
   return {
-    responseStatus,
-    sendLoginRequest
+    tryLogin,
+    tryRegister
   };
 }
