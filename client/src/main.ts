82592da1807d90498ff0c9f5e199a573f2db4ebc
Marcin Mazurkiewicz <marcin@mazurkiewicz.dev>
2021-06-27 07:07:29 +0200

    Removed vue-recaptcha-v3 package

--- client/src/main.ts
+++ client/src/main.ts
@@ -3,18 +3,11 @@ import App from "./App.vue";
 import router from "./router";
 import "@/assets/index.css";
 import VueSmoothScroll from "vue3-smooth-scroll";
-import { VueReCaptcha } from "vue-recaptcha-v3";
 
 createApp(App)
   .use(router)
   .use(VueSmoothScroll, {
     updateHistory: false,
     duration: 300
-  })
-  .use(VueReCaptcha, {
-    siteKey: process.env.VUE_APP_RECAPTCHA,
-    loaderOptions: {
-      autoHideBadge: true
-    }
   })
   .mount("#app");
