faa4ae78a4b13088f447d96955ac0ad666377993
Marcin Mazurkiewicz <marcin@mazurkiewicz.dev>
2021-07-03 08:49:31 +0200

    Change in UserAuthHelper: return LoggedUserDetails object from authentication.getPrincipal() instead of userId extracted from authentication.getDetails()

--- src/main/java/dev/mazurkiewicz/florystyka/auth/UserAuthHelper.java
+++ src/main/java/dev/mazurkiewicz/florystyka/auth/UserAuthHelper.java
@@ -1,30 +1,14 @@
 package dev.mazurkiewicz.florystyka.auth;
 
-import org.springframework.security.core.Authentication;
+import dev.mazurkiewicz.florystyka.user.LoggedUserDetails;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
-
-import java.util.Map;
 
 @Component
 public class UserAuthHelper {
 
-    public long geLoggedUserId() {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        Map<?, ?> details;
-        try {
-            details = (Map<?, ?>) authentication.getDetails();
-
-            Object userId = details.entrySet()
-                    .stream()
-                    .filter(x -> x.getKey().equals("userId"))
-                    .map(Map.Entry::getValue)
-                    .findFirst()
-                    .orElseThrow(() -> new IllegalStateException("Authentication don't have details about userId"));
-            return ((Number) userId).longValue();
-        } catch (ClassCastException e) {
-            throw new IllegalStateException(String.format("Can't read authentication details: %s", e.getMessage()));
-        }
+    public LoggedUserDetails getLoggedUser() {
+        return (LoggedUserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
     }
 
 }
