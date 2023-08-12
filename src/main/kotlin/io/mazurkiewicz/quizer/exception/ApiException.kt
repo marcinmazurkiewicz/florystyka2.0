3d7502f4069df056c230d5663a5f7161c5f3f64b
Marcin Mazurkiewicz <marcin@mazurkiewicz.io>
2023-08-12 14:07:53 +0200

    add custom exception handler

---
+++ src/main/kotlin/io/mazurkiewicz/quizer/exception/ApiException.kt
@@ -0,0 +1,18 @@
+package io.mazurkiewicz.quizer.exception
+
+import org.springframework.http.HttpStatus
+
+sealed class ApiException(
+    val httpStatus: HttpStatus,
+    override val message: String,
+    open val errorCode: String
+) : RuntimeException(message)
+
+open class NotFoundApiException(override val message: String, override val errorCode: String) :
+    ApiException(HttpStatus.NOT_FOUND, message, errorCode)
+
+open class NotValidApiException(override val message: String, override val errorCode: String) :
+    ApiException(HttpStatus.BAD_REQUEST, message, errorCode)
+
+open class UnauthorizedApiException(override val message: String, override val errorCode: String) :
+    ApiException(HttpStatus.UNAUTHORIZED, message, errorCode)
