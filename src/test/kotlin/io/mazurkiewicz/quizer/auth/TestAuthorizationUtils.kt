4870c9491efd9e07be3763554808d8bef0508e59
Marcin Mazurkiewicz <marcin@mazurkiewicz.io>
2023-08-19 09:36:15 +0200

    add authorization utils for tests

---
+++ src/test/kotlin/io/mazurkiewicz/quizer/auth/TestAuthorizationUtils.kt
@@ -0,0 +1,36 @@
+package io.mazurkiewicz.quizer.auth
+
+import io.mazurkiewicz.quizer.auth.user.UserRepresentation
+import org.springframework.core.MethodParameter
+import org.springframework.web.bind.support.WebDataBinderFactory
+import org.springframework.web.context.request.NativeWebRequest
+import org.springframework.web.method.support.HandlerMethodArgumentResolver
+import org.springframework.web.method.support.ModelAndViewContainer
+import java.util.*
+
+
+fun prepareAuthentication(
+    userId: UUID = UUID.randomUUID(),
+    email: String = "user@mail.local"
+): HandlerMethodArgumentResolver {
+    return AuthenticationResolver(userId, email)
+}
+
+
+class AuthenticationResolver(private val userId: UUID, private val email: String) : HandlerMethodArgumentResolver {
+
+    override fun supportsParameter(parameter: MethodParameter): Boolean {
+        return parameter.parameterType.isAssignableFrom(UserRepresentation::class.java)
+    }
+
+    override fun resolveArgument(
+        parameter: MethodParameter,
+        mavContainer: ModelAndViewContainer?,
+        webRequest: NativeWebRequest,
+        binderFactory: WebDataBinderFactory?
+    ): Any {
+        return UserRepresentation(userId, email, true)
+    }
+
+}
+
